aa13652974b459c125189ad0d4e89498a2d2a276
Benjamin Gilliland <73527724+Benegilliland@users.noreply.github.com>
2022-05-06 23:32:08 +0100

    Update file.py

--- file.py
+++ file.py
@@ -4,7 +4,8 @@ file = open(r"C:\Users\bengi\OneDrive\Documents\harry_potter.txt")
 text = file.read()
 file.close()
 
-text = text.lower()
+#Convert all text to lower case and remove special characters
+text = text.lower() 
 text = re.sub("\n", " ", text)
 regex = re.compile('[^a-zA-Z \']')
 text = regex.sub('', text)
@@ -12,14 +13,17 @@ text = regex.sub('', text)
 wordList = text.split(' ')
 wordDict = {}
 
-for word in wordList:
+#Count number of times each word appears, store it in dictionary
+for word in wordList: 
     if word in wordDict.keys():
         wordDict[word] += 1
     else:
         wordDict[word] = 1
 
-wordDict = sorted(wordDict.items(), key=lambda x: x[1], reverse=True)
+#Sort in order of word frequency, with most common words appearing first
+wordDict = sorted(wordDict.items(), key=lambda x: x[1], reverse=True) 
 print("Dictionary length = {}".format(len(wordDict)))
 
-with open(r'harry_potter_corpus.txt', 'w') as f:
+#Output dictionary to text file
+with open(r'harry_potter_corpus.txt', 'w') as f: 
     print(wordDict, file=f)
