efdec31c563383def21f9f08209b33d207ec989c
Benjamin Gilliland <73527724+Benegilliland@users.noreply.github.com>
2022-05-07 02:21:34 +0100

    Update file.py

--- file.py
+++ file.py
@@ -1,29 +1,63 @@
 import re
+import json
+import os
 
-file = open(r"C:\Users\bengi\OneDrive\Documents\harry_potter.txt")
-text = file.read()
-file.close()
+def readText(fileName):
+    file = open(fileName, encoding="utf-8")
+    text = file.read()
+    file.close()
+    return text
 
-#Convert all text to lower case and remove special characters
-text = text.lower() 
-text = re.sub("\n", " ", text)
-regex = re.compile('[^a-zA-Z \']')
-text = regex.sub('', text)
+def readDict(fileName):
+    with open(fileName, 'r', encoding='utf-8') as handle:
+        wordDict = dict(json.load(handle))
+    return wordDict
 
-wordList = text.split(' ')
-wordDict = {}
+def formatText(text):
+    text = re.sub("[，\-()Ｘ（）…！「。？一」．１２３４５６７８９－—：；a-zA-ZＴＰＣＨＭＬＤａＩＷＦＧ〇ｂ[\n\u3000 ]", "", text)
+    return text
 
-#Count number of times each word appears, store it in dictionary
-for word in wordList: 
-    if word in wordDict.keys():
-        wordDict[word] += 1
-    else:
-        wordDict[word] = 1
+def updateDictionary(text, wordDict):
+    wordList = text.split(' ')
+    #Count number of times each word appears, store it in dictionary
+    for word in text: 
+        if word in wordDict.keys():
+            wordDict[word] += 1
+        else:
+            wordDict[word] = 1
+    print("Dictionary length = {}".format(len(wordDict)))
+    return wordDict
 
-#Sort in order of word frequency, with most common words appearing first
-wordDict = sorted(wordDict.items(), key=lambda x: x[1], reverse=True) 
-print("Dictionary length = {}".format(len(wordDict)))
+def sortDictionary(wordDict):
+    #Sort in order of word frequency, with most common words appearing first
+    wordDict = sorted(wordDict.items(), key=lambda x: x[1], reverse=True)
+    wordDict = dict(wordDict)
+    return wordDict
 
-#Output dictionary to text file
-with open(r'harry_potter_corpus.txt', 'w') as f: 
-    print(wordDict, file=f)
+#print("Dictionary length = {}".format(len(wordDict)))
+
+def printToFile(wordDict, fileName):
+    #Output dictionary to text file
+    with open(fileName, 'w', encoding='utf-8') as handle:
+        json.dump(wordDict, handle, ensure_ascii=False)
+
+def createDict(outputFile):
+    printToFile({}, outputFile)
+
+def updateDict(fileName, outputFile):
+    text = readText(fileName)
+    text = formatText(text)
+    wordDict = readDict(outputFile)
+    wordDict = updateDictionary(text, wordDict)
+    wordDict = sortDictionary(wordDict)
+    printToFile(wordDict, outputFile)
+
+def createCorpusFromDirectory(directory, corpus):
+    createDict(corpus)
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.txt'):
+                print("Reading from {}".format(file))
+                updateDict(os.path.join(root, file), corpus)
+
+createCorpusFromDirectory(r"C:\Users\bengi\Calibre Library", r"C:\Users\bengi\Calibre Library\corpus.json")
